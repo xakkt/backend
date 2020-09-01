d5a7c7b2259299878394cd29ce5a7857463f3b71
Manoj <www.msnegi@gmail.com>
2020-09-01 20:50:20 +0530

    homepage

--- controllers/api/homeController.js
+++ controllers/api/homeController.js
@@ -10,6 +10,8 @@ exports.dashboard = async (req, res)=>{
 	try{
 		let categories = await ProductCategory.find().populate('_products').exec();
 		let setting = await Setting.find({key:"home_banner"},'value').exec();
+		console.log(setting.length)
+		if(!setting.length) return res.json({status: "false", message: "No setting found", data: []})
 		if(!categories.length) return res.json({status: "false", message: "No data found", data: categories});
 		return res.json({status: "success", message: "", data: {categories:categories, banner:setting[0].value}});
 		
