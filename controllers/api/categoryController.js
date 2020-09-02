b25ddd67c5c38d4f8d72056aeeec1013b0504c52
Manoj <www.msnegi@gmail.com>
2020-09-02 17:52:42 +0530

    categroy list

--- controllers/api/categoryController.js
+++ controllers/api/categoryController.js
@@ -7,7 +7,7 @@ const { validationResult } = require('express-validator');
 exports.list = async (req, res)=>{
 	
 	  try{
-			let category = await ProductCategory.find().populate('_products').exec();
+			let category = await ProductCategory.find().exec();
 			if(!category.length) return res.json({status: "false", message: "No data found", data: category});
 			return res.json({status: "success", message: "", data: category});
 			
