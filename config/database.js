8165d923e1979e47b9155a3c52b6b3fbb43c73c7
Manoj <www.msnegi@gmail.com>
2020-10-05 09:56:05 +0530

    db changes

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 //Set up mongoose connection
 console.log('in db config');
 const mongoose = require('mongoose');
-var uri = `mongodb://localhost:27017/latest_xakkt`;
+var uri = `mongodb://localhost:27017/xakkt`;
 mongoose.connect(uri, {useCreateIndex: true,useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
 mongoose.Promise = global.Promise;
 
