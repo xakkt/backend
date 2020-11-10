6b2ea643900e8e69df2fcf19d059efe38808accf
Manoj Negi <www.msnegi@gmail.com>
2020-11-10 13:59:25 +0530

    worked on product pricing styling

---
+++ models/product_regular_pricing.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const FKHelper = require('../helper/foreign-key-constraint');
+const Schema = mongoose.Schema;
+
+const productRegularPricingSchema = new Schema({ 
+     _product: {
+        type: Schema.Types.ObjectId,
+        ref:'Product',
+        required:true,
+        validate: {
+                validator: function(v) {
+                return FKHelper(mongoose.model('Product'), v);
+          },
+        message: `Product doesn't exist`
+      }
+    
+    },
+    _user: {
+        type: Schema.Types.ObjectId,
+        ref:'User',
+        required:true,
+        validate: {
+                validator: function(v) {
+                return FKHelper(mongoose.model('User'), v);
+             },
+            message: `User doesn't exist`
+        }
+    },
+    _store: {
+        type: Schema.Types.ObjectId,
+        ref:'Store',
+        required:true,
+        validate: {
+                validator: function(v) {
+                return FKHelper(mongoose.model('Store'), v);
+             },
+            message: `Store doesn't exist`
+        }
+    },
+    product_status: {
+        type: String,
+        enum: ['Active','Inactive'],
+    },
+    regular_price: {
+        type: Number,
+        required: true
+    },
+ },{timestamps:true});
+
+module.exports = mongoose.model('ProductRegularPricing', productRegularPricingSchema);
