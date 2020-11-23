f9be189e6f511c23477529e886a9ec27363cc821
Manoj Negi <www.msnegi@gmail.com>
2020-11-23 18:27:23 +0530

    banner issue

--- controllers/api/bannerController.js
+++ controllers/api/bannerController.js
@@ -8,26 +8,25 @@ exports.bannderproduct = async (req, res) => {
     try {
         var storePrice = []
         var date = moment().utc().format("YYYY-MM-DDTHH:mm:ss.SSS[Z]");
-        let store = await StoreProductPricing.find({$and: [
-            { _store: req.body._store },
-            { _deal: req.body._deal },
-            { deal_start:{$lte:date} },{ deal_end:{$gte:date} }
-          ],  }).populate('_product').select(' -deal_start -deal_end -deal_percentage -createdAt -updatedAt').lean()
-       await Promise.all(store.map( async (element) =>{
-        var data = {}
-          var _store = element._store
-          var _product = element._product._id
-          let productPrice =   await _global.productprice(_store, _product)
-          console.log("---pro",productPrice)
-          data = { ...element}
-           if(productPrice)
-           {
-               data._product.regular_price = productPrice.regular_price
-               data._product.deal_price = productPrice.deal_price
-
-           }
-           delete(data.deal_price)
-           storePrice.push(data)
+        let store = await StoreProductPricing.find({
+            $and: [
+                { _store: req.body._store },
+                { _deal: req.body._deal },
+                { deal_start: { $lte: date } }, { deal_end: { $gte: date } }
+            ],
+        }).populate('_product').select(' -deal_start -deal_end -deal_percentage -createdAt -updatedAt').lean()
+        await Promise.all(store.map(async (element) => {
+            var data = {}
+            var _store = element._store
+            var _product = element._product._id
+            let productPrice = await _global.productprice(_store, _product)
+            data = { ...element }
+            if (productPrice) {
+                data._product.regular_price = productPrice.regular_price
+                data._product.deal_price = productPrice.deal_price
+            }
+            delete (data.deal_price)
+            storePrice.push(data)
         }))
         if (!store.length) return res.json({ status: false, message: "Data not found" })
         return res.json({ status: true, message: "Listing", data: storePrice })
