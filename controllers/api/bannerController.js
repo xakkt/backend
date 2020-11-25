53dd266a29ca1b3004043f8ec916e1047e70004d
Manoj Negi <www.msnegi@gmail.com>
2020-11-25 18:25:21 +0530

    worked on issuess

--- controllers/api/bannerController.js
+++ controllers/api/bannerController.js
@@ -15,6 +15,7 @@ exports.bannderproduct = async (req, res) => {
                 { deal_start: { $lte: date } }, { deal_end: { $gte: date } }
             ],
         }).populate('_product').select(' -deal_start -deal_end -deal_percentage -createdAt -updatedAt').lean()
+        console.log("--value",store)
         await Promise.all(store.map(async (element) => {
             var data = {}
             var _store = element._store
@@ -28,6 +29,7 @@ exports.bannderproduct = async (req, res) => {
             delete (data.deal_price)
             storePrice.push(data)
         }))
+        console.log("--value",storePrice)
         if (!store.length) return res.json({ status: false, message: "Data not found" })
         return res.json({ status: true, message: "Listing", data: storePrice })
 
