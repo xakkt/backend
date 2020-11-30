a8a480c516f2a0e6e1809d6b16c14ca53f9c6f13
Manoj Negi <www.msnegi@gmail.com>
2020-11-30 18:51:26 +0530

    worked on csv file and device token funcality

--- config/database.js
+++
@@ -1,8 +0,0 @@
-//Set up mongoose connection
-console.log('in db config');
-const mongoose = require('mongoose');
-var uri = `mongodb://localhost:27017/xakkt`;
-mongoose.connect(uri, {useCreateIndex: true,useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
-mongoose.Promise = global.Promise;
-
-module.exports = mongoose;
