6f2473ae7c261639ea91e4e2272fa213ae1bfa05
Manoj Negi <www.msnegi@gmail.com>
2020-12-08 17:12:26 +0530

    worked on issues

--- controllers/api/pushController.js
+++ controllers/api/pushController.js
@@ -1,33 +1,63 @@
-const admin  =  require('../../firebase-config');
+const admin = require('../../firebase-config');
 const notification_options = {
     priority: "high",
     timeToLive: 60 * 60 * 24
-  };
-exports.firebase = async(req,res) =>{
-    try{
-    const  registrationToken = req.body.registrationToken
-    // const message = req.body.message
-    const options = {
-        priority: 'high',
-        timeToLive: 60 * 60 * 24, // 1 day
-      };
-      const payload = {
-        notification: {
-          title: 'Notification Title',
-          body: 'This is an example notification',
-        }
-      };
-    admin.messaging().sendToDevice(registrationToken, payload,options)
-    .then( response => {
-       return res.status(200).json({message:"Notification sent successfully",data:response})
-    })
-    .catch( error => {
-        console.log(error);
-    })
-}
-catch(err)
-{
-    console.log('errr',err);
+};
+exports.firebase = async (req, res) => {
+    try {
+        const registrationToken = req.body.registrationToken
+        const message = req.body.message
+        const options = {
+            priority: 'high',
+            timeToLive: 60 * 60 * 24, // 1 day
+        };
+        var payload = {
+			notification: {
+			  title: 'Testing',
+			  body: 'Testing',
+			},
+			data: {
+			  push_type:"1",
+			},
+			// webpush: {
+			//   headers: {
+			// 	Urgency: "high"
+			//   },
+		   notification: {
+				body: "This is a message from FCM to web",
+				requireInteraction: "true",
+				
+			  }
+			// }
+		  
+		}
 
-}
+
+
+
+
+
+
+        // const payload = {
+        //     'notification': {
+        //         'title': 'test',
+        //         'body': 'test',
+        //     },
+        //     // NOTE: The 'data' object is inside payload, not inside notification
+        //     'data': {
+        //         'personSent': 'test'
+        //     }
+        // };
+        admin.messaging().sendToDevice(registrationToken, payload, options)
+            .then(response => {
+                return res.status(200).json({ message: "Notification sent successfully", data: response })
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+    catch (err) {
+        console.log('errr', err);
+
+    }
 }
--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,6 @@
-var admin = require('firebase-admin')
-var serviceAccount = require("./xakkt-grocery.json")
+var admin = require("firebase-admin");
+var serviceAccount =  require("./xakkt-grocery.json");
+var key = 'AAAA0r_CIVo:APA91bEnbhrsj9726TmWMcupZ4KSSetg5pDF2LoXy2j0bnGjYA7-bejiWvkH-WddDxAllgauhJ6_Jm8H-SIal0kcEvkuw-E2yIxyItInHQNAf0PqRFOodjc1kMAjRVNoM9tN3DhpCILS'
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://xakkt-grocery-387b2-default-rtdb.firebaseio.com"
