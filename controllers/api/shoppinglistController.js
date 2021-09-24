8f68ac25006680410d88184e3ac0cb6eb6a2f6bd
Manoj <www.msnegi@gmail.com>
2021-09-24 13:57:01 +0530

    shopping list

--- controllers/api/shoppinglistController.js
+++ controllers/api/shoppinglistController.js
@@ -21,29 +21,29 @@ exports.allShoppingLists = async (req, res) => {
 	}
 },
 
-	exports.addProductToshoppinglist = async (req, res) => {
+exports.addProductToshoppinglist = async (req, res) => {
 
-		const errors = await validationResult(req);
-		if (!errors.isEmpty()) {
-			return res.status(400).json({ errors: errors.array() });
+	const errors = await validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(400).json({ errors: errors.array() });
+	}
+
+	try {
+		shoppinglistInfo = {
+			_shoppinglist: req.body._shoppinglist,
+			_product: req.body._product,
+			quantity: req.body.quantity
 		}
+		const product = await Shoppinglist.create(shoppinglistInfo)
 
-		try {
-			shoppinglistInfo = {
-				_shoppinglist: req.body._shoppinglist,
-				_product: req.body._product,
-				quantity: req.body.quantity
-			}
-			const product = await Shoppinglist.create(shoppinglistInfo)
+		return res.json({ status:1, message: "Product added to shoppinglist successfully", data: product });
 
-			return res.json({ status:1, message: "Product added to shoppinglist successfully", data: product });
+	} catch (err) {
 
-		} catch (err) {
+		res.status(400).json({ data: err.message });
+	}
 
-			res.status(400).json({ data: err.message });
-		}
-
-	};
+};
 
 
 exports.createShoppingList = async function (req, res) {
@@ -105,24 +105,29 @@ exports.shoppinglistProducts = async (req, res) => {
 	try {
 		let shoppinglist = await Shoppinglist.find({ _shoppinglist: req.params.shoplist }).populate({
 																									path:'_product', 
-																									select:'name sku price image weight',
+																									select:'name sku image weight',
 																									populate:{
 																										path:'_unit',
 																										select:'name'
 																									}}).populate('_shoppinglist', 'name _store').lean();
 
-		if (!shoppinglist.length) return res.json({ status:0, message: "No data found", data: shoppinglist });
-
+		if(!shoppinglist.length) return res.json({ status:0, message: "No data found", data: shoppinglist });
+		
 		shoppinglist = await Promise.all(shoppinglist.map(async (list) => {
+
+			var productPrice = await _global.productprice(list._shoppinglist._store, list._product._id)
 
 			if (!list._product) return
 			var productId = list._product._id.toString();
 			let image_path = (list._product.image) ? list._product.image : 'not-available-image.jpg';
 			let image = `${process.env.BASE_URL}/images/products/${image_path}`;
 			let unit = list._product._unit.name;
+			let deal_price = productPrice.deal_price;
+			let regular_price =  productPrice.regular_price;
 			var wishList = await _global.wishList(req.decoded.id, list._shoppinglist._store)
 			var shoppingList = await _global.shoppingList(req.decoded.id, list._shoppinglist._store)
 			var cartProducts = await _global.cartProducts(req.decoded.id, list._shoppinglist._store)
+
 
 			var in_wishlist = (wishList.includes(productId)) ? 1 : 0;
 			var in_shoppinglist = (shoppingList.includes(productId)) ? 1 : 0;
@@ -130,7 +135,7 @@ exports.shoppinglistProducts = async (req, res) => {
 			var list_quantity = list.quantity;
 			delete(list.quantity)
 			delete(list._product._unit)
-			return { ...list, _product: { ...list._product, image: image, unit:unit,is_favourite: in_wishlist, in_shoppinglist: in_shoppinglist, in_cart: quantity, quantity:list_quantity } };
+			return { ...list, _product: { ...list._product, unit:unit,image: image,is_favourite: in_wishlist, in_shoppinglist: in_shoppinglist, deal_price:deal_price, regular_price:regular_price, in_cart: quantity, quantity:list_quantity } };
 		}).filter(Boolean));
 
 		return res.json({ status:1, message: "", data: shoppinglist });
