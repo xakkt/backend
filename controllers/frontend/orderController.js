6a82599c78db24b5398dfbab67b9a0a604557201
manoj-negi <www.msnegi@gmail.com>
2021-12-06 13:46:44 +0530

    tst

--- controllers/frontend/orderController.js
+++ controllers/frontend/orderController.js
@@ -171,8 +171,8 @@ exports.myorder = async (req,res) =>{
             }
         }).populate('_store','name currency').lean({ getters: true });
 
-        var random = Math.floor(Math.random() * 2)
-        var products = await Product.find().skip(random).limit(5).lean()
+        var random = Math.floor(Math.random() * 5)
+        var products = await Product.find().skip(random).limit(2).lean()
 
         return res.render('frontend/order-listing',{orders:order, products:products, store:store})
 
