bc2019bebf68c3f55fec888f0d59d212658e2bfb
manoj negi <techabet.backend@gmail.com>
2021-12-14 18:44:30 +0530

    worked on edit

--- controllers/frontend/userController.js
+++ controllers/frontend/userController.js
@@ -5,7 +5,9 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const jwt = require('jsonwebtoken');
 var moment = require('moment');
-const { validationResult } = require('express-validator');
+const {
+	validationResult
+} = require('express-validator');
 const Device = require('../../models/device')
 var md5 = require('md5');
 
@@ -18,7 +20,9 @@ exports.addAddress = async (req, res) => {
 
 		const errors = await validationResult(req);
 		if (!errors.isEmpty()) {
-			return res.status(400).json({ errors: errors.array() });
+			return res.status(400).json({
+				errors: errors.array()
+			});
 		}
 
 		var address_array = [];
@@ -37,45 +41,84 @@ exports.addAddress = async (req, res) => {
 		})
 		console.log("0--00000", address_array)
 
-		let user = await User.findOneAndUpdate({ _id: req.session.userid }, { $push: { address: address_array } }, { returnOriginal: false }).exec()
-		if(user.address.length==1){
-			await User.update(
-				{
-				  _id: req.session.userid,
-				  "address._id": user.address[0]._id
-				},
-				{ $set: { "address.$.is_default" : true } }
-			 )
+		let user = await User.findOneAndUpdate({
+			_id: req.session.userid
+		}, {
+			$push: {
+				address: address_array
+			}
+		}, {
+			returnOriginal: false
+		}).exec()
+		if (user.address.length == 1) {
+			await User.update({
+				_id: req.session.userid,
+				"address._id": user.address[0]._id
+			}, {
+				$set: {
+					"address.$.is_default": true
+				}
+			})
 		}
-		if (!user) return res.json({ status: 1, message: "Data not found" })
+		if (!user) return res.json({
+			status: 1,
+			message: "Data not found"
+		})
 		res.redirect(req.header('Referer'));
 
 	} catch (err) {
 		console.log("--log", err)
-		return res.status(404).json({ message: err.message })
+		return res.status(404).json({
+			message: err.message
+		})
 	}
 }
 exports.addresslist = async (req, res) => {
 
 	try {
 
-		if(req.query.default){
-			
-			var defaultAddress = await User.findOne({_id:req.decoded.id},{ address: { $elemMatch: { is_default: req.query.default } } }).lean()
-			if(!defaultAddress.address){ return res.json({ status: 0, message: "No default address added for you" }) }
-			return res.json({data:defaultAddress}) 
+		if (req.query.default) {
+
+			var defaultAddress = await User.findOne({
+				_id: req.decoded.id
+			}, {
+				address: {
+					$elemMatch: {
+						is_default: req.query.default
+					}
+				}
+			}).lean()
+			if (!defaultAddress.address) {
+				return res.json({
+					status: 0,
+					message: "No default address added for you"
+				})
+			}
+			return res.json({
+				data: defaultAddress
+			})
 		}
 
-		let user = await User.findOne({ _id: req.decoded.id }, "contact_no email first_name last_name address").select('-_id').lean()
+		let user = await User.findOne({
+			_id: req.decoded.id
+		}, "contact_no email first_name last_name address").select('-_id').lean()
 		// let user = await User.findOne({ _id: req.decoded.id }).select('-_id -password -role_id -coupons -last_login -updatedAt -createdAt -ncrStatus').lean()
 
-		if (!user) return res.json({ status: 0, message: "Data not found" })
-		return res.json({ state: 1, data: user })
+		if (!user) return res.json({
+			status: 0,
+			message: "Data not found"
+		})
+		return res.json({
+			state: 1,
+			data: user
+		})
 
 
 	} catch (err) {
 		console.log("--err", err)
-		return res.status(404).json({ message: err.message })
+		return res.status(404).json({
+			message: err.message
+		})
 
 	}
 
@@ -83,97 +126,166 @@ exports.addresslist = async (req, res) => {
 exports.deleteAddress = async (req, res) => {
 
 	try {
-		let user = await User.findOneAndUpdate({ _id: req.session.userid },
-			{ $pull: { address: { _id: req.params.address } } },
-			{ new: true }).lean()
-		if (!user) return res.json({ status: 0, message: "Data not found" })
+		let user = await User.findOneAndUpdate({
+			_id: req.session.userid
+		}, {
+			$pull: {
+				address: {
+					_id: req.params.address
+				}
+			}
+		}, {
+			new: true
+		}).lean()
+		if (!user) return res.json({
+			status: 0,
+			message: "Data not found"
+		})
 		res.redirect(req.header('Referer'));
 
 
 	} catch (err) {
-		return res.status(404).json({ message: err.message })
+		return res.status(404).json({
+			message: err.message
+		})
 
 	}
 }
-exports.makeDefaultAddress = async(req, res)=>{
-	try {
-		 let resetDefault = await User.update(
-			{
-			  _id: req.session.userid,
-			  //address: { $elemMatch: { is_default: true} }
-			},
-			{ $set: { "address.$[].is_default" : false } }
-		 )
-		 
-		 if(!resetDefault.nModified)return res.json({ state: 0, message: "Could not updated" })
-
-		 let setDefault = await User.update(
-			{
-			  _id: req.session.userid,
-			  "address._id": req.params.address
-			},
-			{ $set: { "address.$.is_default" : true } }
-		 )
-
-		setDefault.nModified && res.redirect(req.header('Referer'));
-		return res.json({ state: 1, message: "Something went wrong" })	
-
-	} catch (err) {
-		return res.status(404).json({ message: err.message })
-
-	}
-},
-exports.updateAddress = async (req, res) => {
-	try {
-		
-		let user = await User.findOneAndUpdate({ 'address._id': req.params.id },
-			{
+exports.makeDefaultAddress = async (req, res) => {
+		try {
+			let resetDefault = await User.update({
+				_id: req.session.userid,
+				//address: { $elemMatch: { is_default: true} }
+			}, {
 				$set: {
-					"address.$.address": req.body.address,
-					"address.$.city": req.body.city,
-					"address.$.name": req.body.name,
-					"address.$.mobile": req.body.mobile,
-					"address.$.country": req.body.country,
-					"address.$.countrycode": req.body.countrycode,
-					"address.$.pincode": req.body.pincode,
-					"address.$.state": req.body.state,
-					"address.$.region": req.body.region,
-					"address.$.location": { coordinates: [req.body.long, req.body.lat] }
+					"address.$[].is_default": false
 				}
-			},
-			// { $push: { address: address_array } }, 
-			{ returnOriginal: false }).exec()
-		if (!user) return res.json({ status: 1, message: "Data not found" })
-		return res.json({ status: 1, data: user })
+			})
 
-	} catch (err) {
-		console.log("--log", err)
-		return res.status(404).json({ message: err.message })
+			if (!resetDefault.nModified) return res.json({
+				state: 0,
+				message: "Could not updated"
+			})
+
+			let setDefault = await User.update({
+				_id: req.session.userid,
+				"address._id": req.params.address
+			}, {
+				$set: {
+					"address.$.is_default": true
+				}
+			})
+
+			setDefault.nModified && res.redirect(req.header('Referer'));
+			return res.json({
+				state: 1,
+				message: "Something went wrong"
+			})
+
+		} catch (err) {
+			return res.status(404).json({
+				message: err.message
+			})
+
+		}
+	},
+exports.updateAddress = async (req, res) => {
+		try {
+
+			let user = await User.findOneAndUpdate({
+					'address._id': req.params.id
+				}, {
+					$set: {
+						"address.$.address": req.body.address,
+						"address.$.city": req.body.city,
+						"address.$.name": req.body.name,
+						"address.$.mobile": req.body.mobile,
+						"address.$.country": req.body.country,
+						"address.$.countrycode": req.body.countrycode,
+						"address.$.pincode": req.body.pincode,
+						"address.$.state": req.body.state,
+						"address.$.region": req.body.region,
+						"address.$.location": {
+							coordinates: [req.body.long, req.body.lat]
+						}
+					}
+				},
+				// { $push: { address: address_array } }, 
+				{
+					returnOriginal: false
+				}).exec()
+			if (!user) return res.json({
+				status: 1,
+				message: "Data not found"
+			})
+			return res.json({
+				status: 1,
+				data: user
+			})
+
+		} catch (err) {
+			console.log("--log", err)
+			return res.status(404).json({
+				message: err.message
+			})
+		}
+
 	}
-
-}
 exports.editaddress = async (req, res) => {
 	try {
-		
-		let user = await User.findOne({ 'address._id': req.params.id },'address').exec()
+
+		let user = await User.findOne({
+			'address._id': req.params.id
+		}, 'address').exec()
 		let address = await user.address.id(req.params.id);
-		
-		if (!address) return res.json({ status: 0, message: "Data not found" })
-		return res.json({status :1,data:address})
+
+		if (!address) return res.json({
+			status: 0,
+			message: "Data not found"
+		})
+		return res.json({
+			status: 1,
+			data: address
+		})
 
 	} catch (err) {
 		console.log("--log", err)
-		return res.status(404).json({ message: err.message })
+		return res.status(404).json({
+			message: err.message
+		})
 	}
 
 }
-exports.editProfile = async (req, res)=>{
-
-	try{
-		return res.render('frontend/edit-profile')
-	}catch(err){
+exports.editProfile = async (req, res) => {
+	try {
+		let user = await User.findOne({
+			_id: req.session.userid
+		}).exec()
+		return res.render('frontend/edit-profile', {
+			user: user
+		})
+	} catch (err) {
 		console.log(err)
-		return res.json({data:err})
+		return res.json({
+			data: err
+		})
+	}
+
+}
+
+exports.updateProfile = async (req, res) => {
+	try {
+		let user = await User.findOne({
+			_id: req.session.userid
+		}).exec()
+		return res.render('frontend/edit-profile', {
+			user: user
+		})
+	} catch (err) {
+		console.log(err)
+		return res.json({
+			data: err
+		})
 	}
 
 }
